abe75e09c2ffa38261e76d1dd8e4ff476e6857c7
BhumikaAggarwal26 <gargbhumi95@gmail.com>
2024-07-20 10:38:38 +0530

    Tables created in DB by Hibernate using Entities - ORM

--- src/main/java/com/scm/contactManager/entities/Contacts.java
+++ src/main/java/com/scm/contactManager/entities/Contacts.java
@@ -1,5 +1,36 @@
 package com.scm.contactManager.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Contacts {
+
+    @Id
+    private String Id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String picture;
+
+    @Column(length = 1000)
+    private String description;
+    private boolean favorite = false;
+    private String websiteLink;
+    private String linkedInLink;
+
+    @ManyToOne
+    private User user;
 
 }
--- src/main/java/com/scm/contactManager/entities/User.java
+++ src/main/java/com/scm/contactManager/entities/User.java
@@ -1,18 +1,39 @@
 package com.scm.contactManager.entities;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.*;
 
-@Entity
+@Entity(name="user")
 @Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     
+    @Id
     private String userId;
+
+    @Column(name="user_name", nullable = false)
     private String name;
+    
+    @Column(unique = true, nullable = false)
     private String email;
     private String password;
+    @Column(length = 1000)
     private String about;
     private String phoneNumber;
+    @Column(length = 1000)
     private String profilePic;
 
     private boolean enabled=false;
@@ -21,4 +42,7 @@ public class User {
 
     private Providers provider = Providers.SELF;
     private String providerUserId;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Contacts> contacts = new ArrayList<>();
 }
